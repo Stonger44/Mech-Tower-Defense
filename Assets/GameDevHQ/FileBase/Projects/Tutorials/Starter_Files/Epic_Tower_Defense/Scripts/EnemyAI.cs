98936fb7ddddb15d4f755a0e37a9a1786d004374
Stonger44 <yakusoku44@gmail.com>
2019-11-03 20:45:38 -0800

    Set up basic AI System.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _endPoint = null;
+
+    private NavMeshAgent _navMeshAgent = null;
+
+    [SerializeField]
+    private int _health = 0;
+    [SerializeField]
+    private int _warFund = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        _navMeshAgent.SetDestination(_endPoint.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
