78be0feba31538b188369f8657b53b6ceac4aabc
Stonger44 <yakusoku44@gmail.com>
2019-11-05 20:03:09 -0800

    Created SpawnPoint, renamed Enemy script, added GameManager and SpawnManager.

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/Enemy.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private GameObject _endPoint = null;
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/GameManager.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/GameManager.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-
-
     // Start is called before the first frame update
     void Start()
     {
