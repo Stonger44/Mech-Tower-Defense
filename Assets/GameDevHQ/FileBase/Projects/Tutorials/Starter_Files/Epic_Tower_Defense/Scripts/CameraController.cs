637f8879d195e61914c77a22a061536432cbfd1d
Stonger44 <yakusoku44@gmail.com>
2019-11-10 22:00:50 -0800

    Dynamic camera boundaries based on zoom kind of works, but needs improvement.

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/CameraController.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/CameraController.cs
@@ -9,7 +9,7 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float _yInput, _yMin, _yMax;
     [SerializeField] private float _zInput, _zMin, _zMax;
     [SerializeField] private float _scrollInput, _scrollSpeed;
-    [SerializeField] private float _fieldOfView, _fieldOfViewMin, _fieldOfViewMax;
+    [SerializeField] private float _fieldOfViewMin, _fieldOfViewMax;
 
     // Start is called before the first frame update
     void Start()
@@ -43,27 +43,48 @@ public class CameraController : MonoBehaviour
             this.transform.Translate(Vector3.up * _yInput);
 
         //Confine camera
-        var xPos = Mathf.Clamp(this.transform.position.x, _xMin, _xMax);
-        var yPos = Mathf.Clamp(this.transform.position.y, _yMin, _yMax);
-        var zPos = Mathf.Clamp(this.transform.position.z, _zMin, _zMax);
-
-        this.transform.position = new Vector3(xPos, yPos, zPos);
+        ConfineCamera();
     }
 
     private void ZoomCamera()
     {
         _scrollInput = Input.GetAxis("Mouse ScrollWheel") * _scrollSpeed * Time.deltaTime;
 
+        Debug.Log("Scroll Input: " + _scrollInput);
+
         //Zoom in/out
         Camera.main.fieldOfView -= _scrollInput;
-
 
         //Confine Zoom
         Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, _fieldOfViewMin, _fieldOfViewMax);
 
-
         //Change transform min/max values based on magnitude of transform:
-        //As zoom increases, min/max values increas
+        //As field of view increases, min/max values decrease
+        //Need to use LERP or something to make this dynamically smooth
+        if (Camera.main.fieldOfView <= 23)
+        {
+            _xMin = -60;
+            _xMax = -44;
+
+            _zMin = -16;
+            _zMax = -2;
+        }
+        else if (Camera.main.fieldOfView > 23 && Camera.main.fieldOfView < 27)
+        {
+            _xMin = -58;
+            _xMax = -47;
+
+            _zMin = -14;
+            _zMax = -4;
+        }
+        else if (Camera.main.fieldOfView >= 27)
+        {
+            _xMin = -54;
+            _xMax = -52;
+
+            _zMin = -12;
+            _zMax = -6;
+        }
     }
 
     private void MouseMoveCamera()
@@ -73,6 +94,15 @@ public class CameraController : MonoBehaviour
         //If (Input.mousPosition.x > )
         //Screen.width
         //transform.translate(Vector3.left * Time.deltaTime);
+    }
+
+    private void ConfineCamera()
+    {
+        var xPos = Mathf.Clamp(this.transform.position.x, _xMin, _xMax);
+        var yPos = Mathf.Clamp(this.transform.position.y, _yMin, _yMax);
+        var zPos = Mathf.Clamp(this.transform.position.z, _zMin, _zMax);
+
+        this.transform.position = new Vector3(xPos, yPos, zPos);
     }
 
 }
