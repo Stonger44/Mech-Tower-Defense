c43594b492b43d35bcd4d027ac323c59b43190bb
Stonger44 <yakusoku44@gmail.com>
2019-11-12 22:01:15 -0800

    Created some scripts for the towers. I apparently changed the Gatling_Gun prefab, so I hope I didn't break anything...

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/Towers/TowerPlacement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    [SerializeField] private GameObject _towerImage;
+
+    private Ray _rayOrigin;
+    private RaycastHit _hitInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Cast a ray from the mouse position on the screen into the game world. Whoa.
+        //Update postiion of decoy tower (follow mouse position)
+        //Check what the raycast hit (if hit placement spot placeholder)
+        _rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(_rayOrigin, out _hitInfo))
+        {
+            _towerImage.transform.position = _hitInfo.point;
+        }
+    }
+}
