de3e8b59af3b3b71ff395228baca8aeb9106440d
Stonger44 <yakusoku44@gmail.com>
2019-11-14 20:22:35 -0800

    Set up input test code for choosing towers to place.

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/Towers/TowerPlacement.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/Towers/TowerPlacement.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class TowerPlacement : MonoBehaviour
 {
+    /*-----Towers-----*\
+     0: Gatling Gun 
+     1: Gatling Gun Upgrade
+     2: Missle Launcher
+     3: Missle Launcher Upgrade
+    \*-----Towers-----*/
     [SerializeField] private List<GameObject> _towerImageList;
-
+    private GameObject _currentTowerImage;
+    public bool IsPlacingTower { get; private set; }
     private Ray _rayOrigin;
     private RaycastHit _hitInfo;
 
@@ -18,13 +25,26 @@ public class TowerPlacement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Test Code
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
+            //Cycle through Tower1, Tower2, and NoTower (null)
+            if (_currentTowerImage == null)
+            {
+                _currentTowerImage = _towerImageList[0]; //Gatling Gun
+            }
+            else if (_currentTowerImage == _towerImageList[0])
+            {
+                _currentTowerImage = _towerImageList[3]; //Missle Launcher
+            }
+            else if (_currentTowerImage == _towerImageList[3])
+            {
+                _currentTowerImage = null;
+            }
 
+            //Update IsPlacingTower boolean appropriately
+            IsPlacingTower = (_currentTowerImage != null) ? true : false;
+            Debug.Log("IsPlacingTower: " + IsPlacingTower);
         }
 
         //Cast a ray from the mouse position on the screen into the game world. Whoa.
